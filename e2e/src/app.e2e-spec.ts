9438af4859f6372f59564e6f26817f0972331be8
Sebastian Gos <gos.sebastian@gmail.com>
2019-09-08 01:29:50 +0200

    app customization

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular8-firebase app is running!');
+    expect(page.getTitleText()).toEqual('ideaxe');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular8-firebase'`, () => {
+  it(`should have as title 'ideaxe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular8-firebase');
+    expect(app.title).toEqual('ideaxe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular8-firebase app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('ideaxe');
   });
 });
